2e56ea6e4159a7ad5ee0a2f735231c582c206d43
Sofia <ehrlich1@mail.ru>
2012-12-16 11:17:44 -0500

    Update syntax/syntax.c

--- syntax/syntax.c
+++ syntax/syntax.c
@@ -11,6 +11,9 @@ static void	mul_expr(void);
 static void	rest_add_expr(void);
 static void	add_expr(void);
 static void	list_expr(void);
+static void	sync_stream(void);
+
+static unsigned int err;
 
 struct stack sp;
 
@@ -18,126 +21,154 @@ static void
 pr_expr(void)
 {
 	int *val;
-	
 	switch (token.type) {
 	
 	case TOKEN_LPARENTH:
 		get_next_token();
 		add_expr();
+		
+		if (err == 1) {
+			goto err;
+		}
 		if (token.type != TOKEN_RPARENTH) {
-			printf("syntax error\n");
+			err = 1;
+			goto err;
 		} else {
 			get_next_token();
 		}
-		return ;
+		goto out;
 		
 	case TOKEN_INTEGER:
 		val = malloc(sizeof(*val));
 		if (val == NULL)
-			return;
+			goto err;
+		
 		*val = token.value;		
 		push_item(&sp, (void *)val);
 		get_next_token();
-		return ;
+		goto out;
 		
 	default:
-		printf("syntax error\n");
-		return ;
+		err = 1;
+		goto err;
 	}
+err:
+	printf("error\n");
+out:
+	return ;
 }
 
 static void
 rest_mul_expr(void)
 {
 	int *op1, *op2;
-	
+	int tmp;
+		
 	while(token.type == TOKEN_ASTERISK || token.type == TOKEN_SLASH) {	
-		switch(token.type) {
-
-		case TOKEN_ASTERISK:
-			get_next_token();
-			pr_expr();
-			op1 = (int *) pop_item(&sp);
-			op2 = (int *) pop_item(&sp);
-			(*op1) = (*op1) * (*op2);
-			push_item(&sp, (void *)op1);
-			free(op2);
-			break;
-			
-		case TOKEN_SLASH:
-			get_next_token();
-			pr_expr();			
-			op2 = (int *)pop_item(&sp);
-			op1 = (int *)pop_item(&sp);
-			(*op1) = (*op1) / (*op2);
-			push_item(&sp, (void *)op1);
-			free(op2);
-			break;
-		/* case TOKEN_PLUS, case TOKEN_MINUS, case TOKEN_RPARENTH, case TOKEN_EOL - empty cases */
+		tmp = token.type;
+		get_next_token();
+		pr_expr();
+		if (err == 1) {
+			goto err_out;
 		}
+		op2 = (int *) pop_item(&sp);
+		op1 = (int *) pop_item(&sp);
+		
+		if (tmp == TOKEN_ASTERISK) {
+			(*op1) = (*op1) * (*op2);
+		} else if (tmp == TOKEN_SLASH) {
+			(*op1) = (*op1) / (*op2);
+		}
+		push_item(&sp, (void *)op1);
+		free(op2);
 	}
+	
+	if (token.type != TOKEN_PLUS && token.type != TOKEN_MINUS &&
+	    token.type != TOKEN_RPARENTH && token.type != TOKEN_EOL) {
+			err = 1;
+			goto err_out;
+	}
+err_out:
+	return ;
 }
 
 static void
 mul_expr(void)
 {
+		
 	switch(token.type) {		
 	case TOKEN_INTEGER:
 	case TOKEN_LPARENTH:
 		pr_expr();
+		if (err == 1) {
+			goto err;
+		}
 		rest_mul_expr();
-		break;
+		goto out;
 	default:
-		return ; 
+		err = 1;
+		goto err;
 	}
+err:
+	printf("error\n");
+out:
+	return ;
 }
 
 static void
 rest_add_expr(void)
 {
 	int *op1, *op2;
-	
-	while (token.type == TOKEN_PLUS || token.type == TOKEN_MINUS) {
+	int tmp;
 		
-		switch (token.type) {
-		case TOKEN_PLUS:
-			get_next_token();
-			mul_expr();
-			op1 = (int *)pop_item(&sp);
-			op2 = (int *)pop_item(&sp);
-			(*op1) = (*op1) + (*op2);
-			push_item(&sp, (void *)op1);
-			free(op2);
-			break;
-			
-		case TOKEN_MINUS:
-			get_next_token();
-			mul_expr();
-			op2 = (int *)pop_item(&sp);
-			op1 = (int *)pop_item(&sp);
-			(*op1) = (*op1) - (*op2);
-			push_item(&sp, (void *)op1);
-			free(op2);
-			break;
-		/* case TOKEN_EOL, case TOKEN_RPARENTH - empty cases */
+	while (token.type == TOKEN_PLUS || token.type == TOKEN_MINUS) {
+		tmp = token.type;
+		
+		get_next_token();
+		mul_expr();
+		if (err == 1) {
+			goto err_out;
 		}
+		op2 = (int *)pop_item(&sp);
+		op1 = (int *)pop_item(&sp);
+		
+		if (tmp == TOKEN_PLUS) {
+			(*op1) = (*op1) + (*op2);
+		} else if (tmp == TOKEN_MINUS) {
+			(*op1) = (*op1) - (*op2);
+		}
+		push_item(&sp, (void *)op1);
+		free(op2);
 	}
+	
+	if (token.type != TOKEN_EOL && token.type != TOKEN_RPARENTH) {
+		err = 1;
+		goto err_out;
+	}
+err_out:
+	return ;
 }
 
 static void
 add_expr(void)
-{
+{	
 	switch(token.type) {
 	case TOKEN_INTEGER:
 	case TOKEN_LPARENTH:
 		mul_expr();
+		if (err == 1) {
+			goto err;
+		}
 		rest_add_expr();
-		break;
-		
+		goto out;		
 	default:
-		printf("syntax error2\n");
-		return ;
+		err = 1;
+		goto err;
 	}
+err:
+	printf("error\n");
+out:
+	return ;
 }
 
 static void
@@ -145,32 +176,45 @@ list_expr(void)
 {	
 	int *c;
 	
-	while (token.type == TOKEN_INTEGER ||token.type == TOKEN_LPARENTH || token.type == TOKEN_EOL) {
-			if (token.type == TOKEN_EOL) {
-				get_next_token();
-				break;
-			}
-			if (token.type == TOKEN_EOF) {
-				return ;
-			}		
-			add_expr();
-			c = (int *)pop_item(&sp);
-			printf("%i\n", (*c));
-			if (token.type != TOKEN_EOL) {
-				/* syntax error */
-				printf("syntax error1\n");
-			}
+	if (err == 1) {
+		sync_stream();
+		sp.n = 0;
+		err = 0;
+		printf("syntax error\n");
 	}
+again:
+	while (token.type == TOKEN_INTEGER ||token.type == TOKEN_LPARENTH || token.type == TOKEN_EOL) {
+		if (token.type == TOKEN_EOL) {
+			get_next_token();
+			continue;
+		}
+		if (token.type == TOKEN_EOF) {
+			return ;
+		}		
+		add_expr();
+		c = (int *)pop_item(&sp);
+		printf("%i\n", (*c));		
+	}
+	
+	sync_stream();
+	goto again;
 }
-
 
 void
 parse(void)
 {
 	stack_init(&sp);
+	err = 0;
+	
 	get_next_token();
 	list_expr();
-	
 	if (token.type == EOF) {
 	}
+}
+
+static void 
+sync_stream(void) 
+{
+	while (token.type != TOKEN_EOL || token.type != TOKEN_EOF)
+		get_next_token();
 }
