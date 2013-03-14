7462976f59057141d8d21e2d6aef187e732850b4
Sofia <ehrlich1@mail.ru>
2013-03-14 19:56:36 +0400

    Update stk.h

--- stack/stk.h
+++ stack/stk.h
@@ -1,25 +1,24 @@
-/*
- * stack header
- */
 #ifndef STK_H
 #define STK_H
 
 #include <stdio.h>
 #include <stdint.h>
-#include <mpl.h>
+
+#include "lex.h"
+#include "mpl.h"
 
 #define OK		0
 #define OVERFLOW	1
 #define SIZE		128
 
 struct stack {
-	mpl_int stack[SIZE];
+	number stack[SIZE];
 	unsigned int n;
 };
 
 int	stk_init(struct stack *sp);
-int	push_item(struct stack *sp, mpl_int *value);
-int	pop_item(struct stack *sp, mpl_int *buf);
+int	push_item(struct stack *sp, number *buf);
+int	pop_item(struct stack *sp, number *buf);
 void	print_stk(struct stack *sp);
 
 #endif /* STK_H */
